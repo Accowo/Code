1ae7c160d3c560e4736f8f590a473a9a09555601
Accowo <Accowo@outlook.com>
2021-12-02 11:25:29 +0800

    1. 增加页面偏移量TOP100排名获取 2. 增加请求延时 3. 修复写文件参数类型错误

--- 猫眼电影排行爬取.py
+++ 猫眼电影排行爬取.py
@@ -2,14 +2,16 @@
 Author: accowo
 Version: default: V1.0
 Date: 2021-10-17 12:50:45
-LastEditTime: 2021-10-18 21:23:56
+LastEditTime: 2021-12-02 11:13:17
 LastEditors: Please set LastEditors
-brief: brief introduction
+brief: 猫眼电影排名爬取
 Attention: something to notice
 '''
 import requests  
 import re
 import json
+import time
+
 #获得网页url源代码
 def get_one_page(url):  
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0Win64x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.47'
@@ -34,14 +36,24 @@ def parse_one_page(html):
                'score': item[5].strip() + item[6].strip()}
 #将提取出的信息写入本地文件			
 def write_to_file(content):
-	with open("result.tex", "a", encoding="utf-8") as f:
-		print(type(json.dumps(content)))
-		f.write(type(json.dumps(content, ensure_ascii=False)+"\n"))
+	with open("result.txt", "a", encoding="utf-8") as f:
+		#json.dumps(content):将content转成str类型
+		#print(type(json.dumps(content)))
+		f.write(json.dumps(content, ensure_ascii=False)+"\n")
+
 #主函数
-def main():  
-	url = 'https://maoyan.com/board/4?offset=0'
+def main(offset):  
+	url = 'https://maoyan.com/board/4?offset='+str(offset)
+	#获得网页url源代码
 	html = get_one_page(url)
-	# print(html)
+	#print(html)
+	#正则表达式解析网页源码并将解析结果写入文件
 	for rank in parse_one_page(html):
 		write_to_file(rank)
-main()
+	print('猫眼电影排名前'+str(offset+10)+'写入文件成功!')
+
+if __name__ == '__main__':
+	#增加偏移量获取TOP100排名
+	for i in range(10):
+		main(offset=i * 10)
+		time.sleep(1)
